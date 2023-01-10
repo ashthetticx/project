4b65c77b4c2aed289ff5f3f5d054dd39a339d110
a4akhare@gmail.com <a4akhare@gmail.com>
2023-01-10 07:37:41 +0530

    hey

--- chaticon/script.js
+++
@@ -1,6 +0,0 @@
-const formWrapper = document.querySelector(".formbold-form-wrapper");
-  const formActionButton = document.querySelector(".formbold-action-btn");
-  function chatboxToogleHandler() {
-    formWrapper.classList.toggle("active");
-    formActionButton.classList.toggle("active");
-  }
